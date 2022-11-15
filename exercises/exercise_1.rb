19b6c8edb4555566ede600a497b248a63b07d957
ikayden <ikyden23@gmail.com>
2022-11-14 18:48:34 -0800

    Exercise 1: Create 3 stores

--- exercises/exercise_1.rb
+++ exercises/exercise_1.rb
@@ -3,14 +3,14 @@ require_relative '../setup'
 puts "Exercise 1"
 puts "----------"
 
-store1 = Store.create(name: "Burnaby", annual_revenue: "300000", men: "true", women: "true")
-store2 = Store.create(name: "Richmond", annual_revenue: "1260000", men: "false", women: "true")
-store3 = Store.create(name: "Gastown", annual_revenue: "190000", men: "true", women: "false")
+store1 = Store.create(name: "Burnaby", annual_revenue: 300000, mens_apparel: true, womens_apparel: true)
+store2 = Store.create(name: "Richmond", annual_revenue: 1260000, mens_apparel: false, womens_apparel: true)
+store3 = Store.create(name: "Gastown", annual_revenue: 190000, mens_apparel: true, womens_apparel: false)
 
-puts store1
-puts store2
-puts store3
+puts store1.name
+puts store2.name
+puts store3.name
 
-# puts Store.all.count
+puts Store.all.count
 
 # Your code goes below here ...
