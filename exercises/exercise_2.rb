cb33e47255681f0e41314695cd1e51fab7aff1cc
ikayden <ikyden23@gmail.com>
2022-11-14 18:56:41 -0800

    Exercise 2: Update the first store

--- exercises/exercise_2.rb
+++ exercises/exercise_2.rb
@@ -4,4 +4,17 @@ require_relative './exercise_1'
 puts "Exercise 2"
 puts "----------"
 
-# Your code goes here ...
+#1
+@store1 = Store.first
+puts @store1.id
+p @store1
+
+#2
+@store2 = Store.second
+puts @store2.id
+p @store2
+
+#3
+
+@store1.update(name: "Gucci")
+p @store1
