a28d7898ba75cce4574ad204e4451cbe606ea264
ikayden <ikyden23@gmail.com>
2022-11-14 19:00:34 -0800

    Exercise 3: Delete the third store

--- exercises/exercise_1.rb
+++ exercises/exercise_1.rb
@@ -7,10 +7,10 @@ store1 = Store.create(name: "Burnaby", annual_revenue: 300000, mens_apparel: tru
 store2 = Store.create(name: "Richmond", annual_revenue: 1260000, mens_apparel: false, womens_apparel: true)
 store3 = Store.create(name: "Gastown", annual_revenue: 190000, mens_apparel: true, womens_apparel: false)
 
-# puts store1.name
-# puts store2.name
-# puts store3.name
+puts store1.name
+puts store2.name
+puts store3.name
 
-# puts Store.all.count
+puts Store.all.count
 
 # Your code goes below here ...
